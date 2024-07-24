211967306ab02640c2afcb3c62b71ba383b7c692
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-07-24 19:57:10 +0530

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Search from "./components/Search";
 import Signup from "./pages/Signup";
 import WatchList from "./components/WatchList";
 
 const App = () => {
-  const isAuthenticated = localStorage.getItem("email");
+  const [isAuthenticated, ] = useState(!!localStorage.getItem("email"));
 
   return (
     <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Router>
           <Routes>
-            <Route path="/" element={<Navigate to="/form" />} />
+            <Route path="/" element={isAuthenticated ? <Navigate to="/search" /> : <Navigate to="/form" />} />
             <Route path="/form" element={<Signup />} />
-            <Route path="/watchlist" element={isAuthenticated ? <WatchList /> : <Navigate to="/form" />} />
-            <Route path="/search" element={isAuthenticated ? <Search /> : <Navigate to="/form" />} />
+            <Route path="/watchlist" element={<WatchList />} />
+            <Route path="/search" element={<Search />} />
           </Routes>
         </Router>
       </div>
