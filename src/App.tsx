daba3e87f0a004b877938a21a0f341b82c2db3a5
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-07-24 19:59:13 +0530

    auth eroor

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
+
 import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Search from "./components/Search";
 import Signup from "./pages/Signup";
 import WatchList from "./components/WatchList";
 
 const App = () => {
-  const [isAuthenticated, ] = useState(!!localStorage.getItem("email"));
+
 
   return (
     <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Router>
           <Routes>
-            <Route path="/" element={isAuthenticated ? <Navigate to="/search" /> : <Navigate to="/form" />} />
+            <Route path="/" element={ <Navigate to="/form" />} />
             <Route path="/form" element={<Signup />} />
             <Route path="/watchlist" element={<WatchList />} />
             <Route path="/search" element={<Search />} />
