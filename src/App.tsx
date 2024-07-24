3bd8b8bf01f561d8e7bf594f220979c0b090d608
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-07-24 19:13:51 +0530

    form error

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,15 @@ import Signup from "./pages/Signup";
 import WatchList from "./components/WatchList";
 
 const App = () => {
-  const isAuthenticated = Boolean(localStorage.getItem("email"));
-
-  console.log("Is Authenticated:", isAuthenticated); // Debugging line
-
   return (
     <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Router>
           <Routes>
-            <Route path="/" element={<Navigate to={isAuthenticated ? "/search" : "/form"} />} />
+            <Route path="/" element={<Navigate to="/form" />} />
             <Route path="/form" element={<Signup />} />
-            <Route path="/watchlist" element={isAuthenticated ? <WatchList /> : <Navigate to="/form" />} />
-            <Route path="/search" element={isAuthenticated ? <Search /> : <Navigate to="/form" />} />
+            <Route path="/watchlist" element={<WatchList />} />
+            <Route path="/search" element={<Search />} />
           </Routes>
         </Router>
       </div>
